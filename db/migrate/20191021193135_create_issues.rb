0ce66b7c014dc0ac6b2653b02a7847a5dcbde366
Shawn <shawntennity@gmail.com>
2019-10-21 15:40:45 -0400

    Initial commit.  Created Issue-Tracker_API and added tables.  Added associations to models.  Added Index JSON rendering to controllers.

---
+++ db/migrate/20191021193135_create_issues.rb
@@ -0,0 +1,13 @@
+class CreateIssues < ActiveRecord::Migration[6.0]
+  def change
+    create_table :issues do |t|
+      t.string :title
+      t.string :description
+      t.string :creator
+      t.boolean :open_status
+      t.datetime :resolved_date
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,38 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2019_10_21_193304) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "issue_id", null: false
+    t.string "description"
+    t.string "commentor"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["issue_id"], name: "index_comments_on_issue_id"
+  end
+
+  create_table "issues", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "creator"
+    t.boolean "open_status"
+    t.datetime "resolved_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "comments", "issues"
+end
