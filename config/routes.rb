e4a5c3908afea278f998ce3e638269178f22d8e9
Shawn <shawntennity@gmail.com>
2019-10-23 10:36:57 -0400

    Renamed open/closed issues routes and reordered to stop from hitting show action first.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'issues/open', to: 'issues#open_issues'
+      get 'issues/closed', to: 'issues#closed_issues'
       resources :comments
       resources :issues, only: [:index, :show, :create, :updated, :destroy] do
         get 'comments', to: 'issues#issue_comments'
       end
-      get 'open_issues', to: 'issues#open_issues'
-      get 'closed_issues', to: 'issues#closed_issues'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
