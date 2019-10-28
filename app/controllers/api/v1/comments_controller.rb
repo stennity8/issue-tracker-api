8e1b76a748723ab90b3107a0c3fbcdbc6689859b
Shawn <shawntennity@gmail.com>
2019-10-28 17:51:28 -0400

    Added create action for comments.

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -11,6 +11,12 @@ class Api::V1::CommentsController < ApplicationController
     render json: comment
   end
 
+  def create
+    comment = Comment.create(comment_params)
+
+    render json: comment
+  end
+
   private 
 
   def comment_params
